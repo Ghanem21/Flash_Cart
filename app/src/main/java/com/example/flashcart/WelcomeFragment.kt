09730309b198477b9fcfb0c980f5c191fa2c6314
Abdelrhman-Salah-Ghanem <ghanem.ana.abdo@gmail.com>
2022-10-10 01:05:41 +0200

    welcome fragment with bug

--- app/src/main/java/com/example/flashcart/WelcomeFragment.kt
+++ app/src/main/java/com/example/flashcart/WelcomeFragment.kt
@@ -1,20 +1,16 @@
 package com.example.flashcart
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import com.example.flashcart.databinding.FragmentWelcomeBinding
+
 
 class WelcomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val binding: FragmentWelcomeBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_welcome, container, false)
-
-        return binding.root
+        return inflater.inflate(R.layout.fragment_welcome, container, false)
     }
 }
