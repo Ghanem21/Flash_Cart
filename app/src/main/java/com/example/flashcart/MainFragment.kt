cf1af77535e4c4e8c9a60871bcd6eb911a1e05cb
Abdelrhman-Salah-Ghanem <ghanem.ana.abdo@gmail.com>
2022-12-02 01:53:38 +0200

    bottom_navigation

--- app/src/main/java/com/example/flashcart/MainFragment.kt
+++ app/src/main/java/com/example/flashcart/MainFragment.kt
@@ -1,6 +1,8 @@
 package com.example.flashcart
 
+import android.app.Dialog
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,18 +10,20 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.example.flashcart.databinding.FragmentMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 
 class MainFragment : Fragment() {
-    private lateinit var binding:FragmentMainBinding
-    private lateinit var navController:NavController
+    private lateinit var binding: FragmentMainBinding
+    private lateinit var navController: NavController
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_main, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
         val navHostFragment =
             childFragmentManager.findFragmentById(R.id.second_nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
@@ -28,6 +32,23 @@ class MainFragment : Fragment() {
             navController.navigate(item.itemId)
             return@setOnItemSelectedListener true
         }
+
+        binding.checkoutButton.setOnClickListener {
+            val dialog = MaterialAlertDialogBuilder(requireContext(),R.style.CheckoutDialogTheme)
+                .setView(layoutInflater.inflate(R.layout.dialog_checkout, null))
+                .setNegativeButton(getString(R.string.cancel)) { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .setPositiveButton(getString(R.string.checkout)) { _, _ ->
+                    findNavController().navigate(R.id.action_mainFragment_to_checkoutFragment)
+                }
+                .show()
+
+            dialog.getButton(Dialog.BUTTON_NEGATIVE).setTextSize(TypedValue.COMPLEX_UNIT_SP, 21.0f)
+            dialog.getButton(Dialog.BUTTON_POSITIVE).setTextSize(TypedValue.COMPLEX_UNIT_SP, 21.0f)
+
+        }
+
         return binding.root
     }
 }
