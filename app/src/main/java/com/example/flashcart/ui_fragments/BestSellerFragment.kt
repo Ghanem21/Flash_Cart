72f118f1d6e7c7d2012e192d9c24350a73becc1e
Abdulrahman Mohammad Mahmoud Ragab <76666355+Abdulrahman1890@users.noreply.github.com>
2022-12-23 01:57:55 +0200

    all recycle views and adapters

---
+++ app/src/main/java/com/example/flashcart/ui_fragments/BestSellerFragment.kt
@@ -0,0 +1,41 @@
+package com.example.flashcart.ui_fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.flashcart.R
+import com.example.flashcart.ui_fragments.adapters_and_data.ProductsAdapter
+import com.example.flashcart.ui_fragments.adapters_and_data.ProductsObjects
+
+
+class BestSellerFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+        ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_products_details, container, false)
+        setupRecyclerView(view)
+        return view
+    }
+
+    private fun setupRecyclerView(view: View?) {
+        val context = requireContext()
+        val productAdapter = ProductsAdapter(ProductsObjects.productsList!!)
+
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.products_details_recycler_view)
+        recyclerView?.adapter = productAdapter
+        recyclerView?.setHasFixedSize(true)
+
+        val layoutManager = LinearLayoutManager(context)
+        layoutManager.orientation = RecyclerView.VERTICAL
+        recyclerView?.layoutManager = layoutManager
+    }
+
+}
